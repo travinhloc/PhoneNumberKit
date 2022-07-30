05c4b32b44e6a8c5c6d7933598e8ce7f3b115d60
loctraSmc <t.loc@servicemycar.com>
2022-07-30 21:37:46 +0400

    get resource name

--- lib/src/main/java/me/ibrahimsn/lib/api/Country.kt
+++ lib/src/main/java/me/ibrahimsn/lib/api/Country.kt
@@ -1,7 +1,18 @@
 package me.ibrahimsn.lib.api
 
+import android.content.Context
+
 data class Country(
     val iso2: String,
     val name: String,
     val code: Int,
-)
+){
+    fun getFlagResource(context: Context): Int {
+        return context.resources.getIdentifier(
+            "country_flag_$iso2",
+            "drawable",
+            context.packageName
+        )
+    }
+
+}
